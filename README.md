088f1d68dede985202a35089c00af0d365a2af29
Allen Guo <guosonglin001@gmail.com>
2024-10-12 10:37:50 +0800

    init project (#1)

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# arm-toolchain
-aarch64 toolchain
+# aarch64-toolchain
+
+aarch64 gnu toolchain
+
+## target
+
+clang + ld.lld + sysroot(copy from linux) on macos
+
+## build
+
+```bash
+# setup env
+scripts/env_setup.sh
+
+# llvm
+bash scripts/llvm_download.sh
+bash scripts/llvm_build.sh
+```
+
+check `sysroot/sysroot_ubuntu_22_04.sh`
---
+++ scripts/llvm_download.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+LLVM_SHA="release/19.x"
+LLVM_REPO="https://github.com/llvm/llvm-project.git"
+
+git clone -n --depth=1 --filter=tree:0 $LLVM_REPO llvm-project
+pushd llvm-project
+git sparse-checkout set --no-cone \
+  /clang /cmake /compiler-rt /libcxx /libcxxabi /libunwind /lld /lldb /llvm /runtimes \
+  '!/clang/test' '!/clang/unittests' '!/clang/docs' '!/clang/www' \
+  '!/llvm/test' '!/llvm/unittests' '!/llvm/docs' \
+  '!/compiler-rt/test' '!/libcxx/test' '!/lld/test' '!/lldb/test' '!/lldb/unittests' \
+  '/.gitignore'
+git sparse-checkout add \
+  '!/llvm/lib/Target' '/llvm/lib/Target/*.*' \
+  '/llvm/lib/Target/RISCV' '/llvm/lib/Target/ARM' '/llvm/lib/Target/AArch64'
+git fetch --depth 1 origin $LLVM_SHA
+git checkout
+popd
